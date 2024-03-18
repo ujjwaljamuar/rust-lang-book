9dbdd2c32673d2b80930aec731e19e143c1344b1
Ujjwal Jamuar <ujjwalj12222@gmail.com>
2024-03-18 16:04:30 +0530

    enums and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,22 @@
-fn main(){}
+fn main(){
+    // define enum Direction
+    #[derive(Debug)]
+    enum Direction {
+        North,
+        East,
+        South,
+        West,
+    }
+
+    // initialize and access enum variants
+    let north = Direction::North;
+    let east = Direction::East;
+    let south = Direction::South;
+    let west = Direction::West;
+
+    // print enum values
+    println!("{:?}", north);
+    println!("{:?}", east);
+    println!("{:?}", south);
+    println!("{:?}", west);
+}
